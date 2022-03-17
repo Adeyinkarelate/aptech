6d894e9ce95f056523ef374c3ef43e3dd6c36d8b
adeyinkarelate <mailadeyinka2020@gmail.com>
2022-03-17 12:33:19 +0100

    added new password

--- aptech/settings.py
+++ aptech/settings.py
@@ -151,7 +151,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST ='smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'aptechotacentre@gmail.com'
-EMAIL_HOST_PASSWORD = 'aptech1234'
+EMAIL_HOST_PASSWORD = 'aptech12345'
 EMAIL_USE_TLS = True
 
 cloudinary.config( 
